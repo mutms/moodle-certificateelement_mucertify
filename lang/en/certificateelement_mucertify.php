9b100c8c96bcaf39af8eeea73d0837b593bf2ba6
Petr Skoda <petr@mutms.org>
2025-04-13 09:13:42 +0200

    Fix license

--- lang/en/certificateelement_mucertify.php
+++ lang/en/certificateelement_mucertify.php
@@ -1,5 +1,5 @@
 <?php
-// Certifications fields plugin for Certificate plugin for Moodle™.
+// This file is part of Certifications fields plugin for Certificate plugin for Moodle™.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
