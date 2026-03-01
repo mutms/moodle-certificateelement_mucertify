5de2799f6acab5009f4b9d8546baf49f763a59d4
Petr Skoda <petr@mutms.org>
2026-03-01 09:14:54 +0100

    Prepare release

--- version.php
+++ version.php
@@ -28,14 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'certificateelement_mucertify';
-$plugin->version = 2026021245;
+$plugin->version = 2026030145;
 $plugin->requires = 2024100700;
-$plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.10.01+';
+$plugin->release = 'v4.5.10.02';
 
 $plugin->dependencies = [
-    'tool_mucertify' => 2026021245,
+    'tool_mucertify' => 2026030145,
     'tool_certificate' => 2025031800,
 ];
